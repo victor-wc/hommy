b578c9d7f2693467c92b742e71eef6169ce74f82
vwc2012 <victorwohlers@hotmail.com>
2020-07-27 14:27:57 -0300

    Trab Ts

--- hommyfront/src/app/home/home.page.ts
+++ hommyfront/src/app/home/home.page.ts
@@ -1,4 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, RendererFactory2 } from '@angular/core';
+import { mdTransitionAnimation } from '@ionic/angular';
+
+class Republica {
+  endereco: string;
+  rating: string;
+  nome: string;
+  descricao: string;
+  imagem: string;
+  imagemRating: string;
+
+
+  
+}
 
 @Component({
   selector: 'app-home',
@@ -6,10 +19,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  republicas: Republica[];
 
+  
   constructor() { }
 
   ngOnInit() {
+    this.republicas = [
+      
+      {
+      endereco: 'Rua Dona Mariana',
+      rating: '4,917',
+      nome: 'Renato',
+      descricao: 'Possui 4 quartos e vaga para carro',
+      imagemRating: "../assets/rating.png",
+      imagem: "../assets/quarto.jpg"
+
+
+    },
+    
+    {
+      endereco: 'Rua Siqueira Campos',
+      rating: '4,815',
+      nome: 'Victor',
+      descricao: 'Piscina e vaga para carro',
+      imagemRating: "../assets/rating.png",
+      imagem: "../assets/quarto2.jpg"
+    }
+    
+  ];
+
+
+
   }
 
 }
